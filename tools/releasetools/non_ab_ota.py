bb6b1f8977037c51e248ae9896a5d07bd3d481d5
KreAch3R <kreach3r@users.noreply.github.com>
2022-03-16 11:22:19 +0530

    build: Project Blaze Flashing Script alongwith some useful information
    * Credits to zephiK and Chroma for the original code
    * Fixed Spacing
    * Used krexus-caf/build@5c602f2 commit from krexus,
    
    * Citrus-CAF Edits:
      * Show device name next to ROM's name
      * Fix for Devices without ro.product.model prop from Citrus-CAF/build@cdcb576
      * Adapted to Android 9 PIE GetProperty logic from PixelExperience-P/build@800d29c
    
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -214,6 +214,26 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
+  date = target_info.GetBuildProp("ro.blaze.build.date")
+  version = target_info.GetBuildProp("ro.blaze.version")
+
+  if target_info.GetBuildProp("ro.product.model") is not None:
+    model = target_info.GetBuildProp("ro.product.model")
+    script.Print("***********************************************");
+    script.Print("           Project Blaze for %s"%(model));
+    script.Print("               BY: ＡＤＩＴＹＡ                ");
+    script.Print("   VERSION: %s"%(version));
+    script.Print("   COMPILED ON: %s"%(date));
+    script.Print("***********************************************");
+  else:
+    name = target_info.GetBuildProp("ro.product.name")
+    script.Print("***********************************************");
+    script.Print("           Project Blaze for %s"%(name));
+    script.Print("               BY: ＡＤＩＴＹＡ                ");
+    script.Print("   VERSION: %s"%(version));
+    script.Print("   COMPILED ON: %s"%(date));
+    script.Print("***********************************************");
+
   device_specific.FullOTA_InstallBegin()
 
   CopyInstallTools(output_zip)
